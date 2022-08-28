915290775d928e174e099254bb28f934dcbd3334
Carmine Watcher <cwatcher@student.21-school.r>
2022-08-28 17:12:06 +0300

    cwatcher/tt.sh: double->single died

--- philo/cwatcher/tt.sh
+++ philo/cwatcher/tt.sh
@@ -1,6 +1,20 @@
-# $1 - output suffix
-# $2..$6 - philo args
+# First arg - subsuffix of a filename,
+# where output of a philo app is redirected to.
+# Next args are passed to a philo app.
+#
+# Output of the script are "is died" line
+# and the last line.
+
 [ -d log ] || mkdir log
 OUTFILE=log/$2_$3_$4_$5_$6.$1.log
 ./philo $2 $3 $4 $5 $6 > $OUTFILE
-echo $(grep died $OUTFILE && echo ';') $(tail -n1 $OUTFILE)
+
+DEATH_MESSAGE=$(grep d $OUTFILE)
+LAST_MESSAGE=$(tail -n1 $OUTFILE)
+
+if [[ $DEATH_MESSAGE == "" || $DEATH_MESSAGE == $LAST_MESSAGE ]]; then
+	DEATH_MESSAGE=""
+else
+	DEATH_MESSAGE+="; "
+fi
+echo "$DEATH_MESSAGE$LAST_MESSAGE"
